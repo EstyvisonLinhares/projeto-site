5dd9f19417a49182e68c1f0c00805daed72d363b
Tito3388 <estyvison.linhares@pitang.com>
2022-05-20 06:53:16 -0300

    Projeto iniciado

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createServer, Model } from 'miragejs';
+import { App } from './App';
+
+createServer({
+  models: {
+    customer: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      customers: [
+        {
+          id: 1,
+          name: 'Maria',
+          age: '20',
+          service: 'Corte de cabelo',
+          price: 'R$20'
+        },
+        {
+          id: 2,
+          name: 'Luiza',
+          age: '25',
+          service: 'Chapinha',
+          price: 'R$30'
+        },
+        {
+          id: 3,
+          name: 'Joseane',
+          age: '50',
+          service: 'Botox',
+          price: 'R$40'
+        },
+      ]
+    })
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
